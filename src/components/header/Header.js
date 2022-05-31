7fbe16621f5473b8fb163ac47a66237b15467272
Madhur Mata <you@example.com>
2022-05-31 11:31:52 +0800

    back button

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function Header() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  console.log('params', location);
+
   return (
     <header className="header">
       <h1>Heroes App</h1>
+      {location.pathname === '/' ? null : (
+        <div
+          className="back-button"
+          onClick={() => navigate(-1)}
+          role="button"
+          tabIndex={0}
+          onKeyDown={() => navigate(-1)}>
+          <h5>Home</h5>
+        </div>
+      )}
     </header>
   );
 }
